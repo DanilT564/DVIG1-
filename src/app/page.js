242eb97d9649d54a84945deee5911b401040b6d4
Danil-564 <timeev_danil3@mail.ru>
2025-05-02 04:49:16 +0500

    55

--- src/app/page.js
+++ src/app/page.js
@@ -1,1 +1,8 @@
- 
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">DVIG Shop</h1>
+      <p>Welcome to our online store</p>
+    </main>
+  )
+} 
